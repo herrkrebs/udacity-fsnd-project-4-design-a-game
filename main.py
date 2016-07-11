5cdc5f3493c3b449feb4240eb5de82f42ba99378
Kai Mies <develop@regnavi.de>
2016-07-11 19:47:45 +0200

    Improve user email notification

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import webapp2
 from google.appengine.api import mail, app_identity
 
 from models.game import Game, GameState
+from models.user import User
 
 
 class SendReminderEmail(webapp2.RequestHandler):
@@ -15,21 +16,24 @@ class SendReminderEmail(webapp2.RequestHandler):
         """Send a reminder email to each User with an email about games.
         Called every hour using a cron job"""
         app_id = app_identity.get_application_id()
-        games = Game.query(Game.state == GameState.ACTIVE)
 
-        for user_key, group in groupby(games, lambda game: game.active_player):
-            user = user_key.get()
-            subject = 'This is a reminder!'
-            body = 'Hello {}, it\'s your turn!'.format(
-                user.name)
+        users = User.query()
+        for user in users:
+            games = Game.query(Game.active_player == user.key).filter(
+                Game.state == GameState.ACTIVE)
 
-            # This will send notification emails,
-            # the arguments to send_mail are:
-            # from, to, subject, body
-            mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
-                           user.email,
-                           subject,
-                           body)
+            if games:
+                subject = 'This is a reminder!'
+                body = 'Hello {}, it\'s your turn!'.format(
+                    user.name)
+
+                # This will send notification emails,
+                # the arguments to send_mail are:
+                # from, to, subject, body
+                mail.send_mail('noreply@{}.appspotmail.com'.format(app_id),
+                               user.email,
+                               subject,
+                               body)
 
 
 app = webapp2.WSGIApplication([
